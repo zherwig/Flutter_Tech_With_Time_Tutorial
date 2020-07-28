749480f962af36a53feeaddef70fe00357407f27
Zjef Herwig <zherwig@hotmail.com>
2020-07-28 18:57:13 +0300

    Menu bar and text on page widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 
+//Main program starting point
 void main() {
   runApp(MyApp());
 }
 
+// statelessWidget used for static widgets
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tims App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.red,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        //adapt to screen pixel count
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ,
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //widget (text), within widget (Appbar) within widget (Scaffold)
+    return Scaffold(
+        appBar: AppBar(title: Text("Hello world")),
+        body: Text("This is my first app"));
   }
 }
